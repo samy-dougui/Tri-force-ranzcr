1b8bbe633990042afdace8c2d3e5a300c3abf386
Dougui Samy <dougui_sam@mfopt10.metz.supelec.fr>
2021-03-10 17:48:01 +0100

    Refactor utils.py

--- utils.py
+++ utils.py
@@ -1,48 +1,133 @@
 import numpy as np
 import pandas as pd
-import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from sklearn import metrics
 import os
 import cv2
 import timm
 import matplotlib.pyplot as plt
 from datetime import datetime
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from torchsummary import summary
 
+from models import EfficientNet
 
 def get_config():
-    cfg = {
+    return {
         "BATCH_SIZE": 32,
         "EPOCHS": 10,
-        "IMAGE_SIZE": 528,
+        "IMAGE_SIZE":{
+            "efficientnet": 528,
+            "resnet": 256
+        },
         "TARGET_SIZE": 11,
+        "models":{
+            "resnet":"./models/",
+            "efficientnet":"./models/",
+            "ensemble":"./models/"
+        },
         "TARGET_COLS": ['ETT - Abnormal', 'ETT - Borderline', 'ETT - Normal',
                         'NGT - Abnormal', 'NGT - Borderline', 'NGT - Incompletely Imaged', 'NGT - Normal',
                         'CVC - Abnormal', 'CVC - Borderline', 'CVC - Normal',
                         'Swan Ganz Catheter Present'],
         "IMAGE_COL": "StudyInstanceUID",
         "DATASET_DIR": "../input/ranzcr-clip-catheter-line-classification/",
-        "MODEL_NAME": "efficientnet_b6",
+        "MODEL_NAME": "efficientnet_b7",
         "LR": 0.01,
         "WEIGHT_DECAY": 0.0,
-        "TRAIN_VALIDATION_FRAC": 0.8
+        "TRAIN_VALIDATION_FRAC": 0.9
 
     }
-    return cfg
+
+class DatasetTransformer(torch.utils.data.Dataset):
+    def __init__(self, df, transform, cfg):
+
+        self.transform = transform
+        self.df = df
+        self.cfg = cfg
+        self.labels = self.df[self.cfg["TARGET_COLS"]].values
+        self.file_names = df[self.cfg["IMAGE_COL"]].values
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        image_name = self.file_names[idx]
+        image_path = f'{self.cfg["DATASET_DIR"]}/train/{image_name}.jpg'
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        image_t = self.transform(image=image)["image"]
+        data = {
+            "image": image_t.float(),
+            "targets": torch.tensor(self.labels[idx]).float()
+        }
+
+        return data
+
+
+class DatasetTransformer_prediction(torch.utils.data.Dataset):
+    def __init__(self, df, transform, cfg):
+        
+        self.transform = transform
+        self.df = df
+        self.cfg = cfg
+        self.file_names = self.df[self.cfg["IMAGE_COL"]].values
+        
+    def __len__(self):
+        return len(self.df)
+    
+    def __getitem__(self, idx):
+        image_name = self.file_names[idx]
+        image_path = f'{self.cfg["DATASET_DIR"]}/test/{image_name}.jpg'
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        image_t = self.transform(image=image)['image']
+        data = { 
+            "image": image_t.float(), 
+        }
+        
+        return data
+
+
+class ModelCheckpoint:
+    def __init__(self, model, filepath=None, filename=None):
+        self.min_loss = None
+        self.model = model
+
+        if filepath is None:
+            filepath = os.path.join(get_current_dir(), "Model Saved")
+
+        if filename is None:
+            filename = "best_model.pt"
+
+        self.filepath = os.path.join(filepath, filename)
+
+    def update(self, loss):
+        if (self.min_loss is None) or (loss < self.min_loss):
+            print(f"\tSaving a better model here: {self.filepath}", end='\n')
+            torch.save(self.model, self.filepath)
+            self.min_loss = loss
+
 
 
 def get_data(mode="test", percentage=0.3):
-    data = pd.read_csv(
-        '/kaggle/input/ranzcr-clip-catheter-line-classification/train.csv')
+    try:
+        data = pd.read_csv(
+            './input/ranzcr-clip-catheter-line-classification/train.csv')
+    except Exception:
+        print("Please put the training dataset here: ./input/ranzcr-clip-catheter-line-classification/train.csv")
+        return 
     if mode != "training":
-        data = data.sample(frac=percentage)
-    else:
-        percentage = 1
-    print(f"{mode} mode used, percentage of the data used: {percentage*100}%")
-    return data.sample(frac=percentage)
+        print(f"[{mode}] mode: Using only {percentage}% of the data")
+        return data.sample(frac=percentage)
+
+    print("Training mode, using all the data")
+    return data
 
 
 def get_device():
@@ -52,10 +137,19 @@ def get_device():
 
 
 def get_transform(image_size, augmented=False):
-    return transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Resize((image_size, image_size)),
-    ])
+    if augmented:
+        return A.Compose([ 
+            A.Resize(image_size, image_size),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.5),
+            ToTensorV2()
+        ])
+
+    return  A.Compose([ 
+            A.Resize(image_size, image_size),
+            ToTensorV2()
+        ]) 
+
 
 
 def compute_class_freqs(labels):
@@ -90,13 +184,12 @@ def mean_roc_auc(targets, ouputs, target_size=11):
     for k in range(target_size):
         try:
             roc_auc.append(metrics.roc_auc_score(targets[:, k], ouputs[:, k]))
-        except Exception as e:
+        except Exception:
             roc_auc.append(0.5)
-            pass
     return np.mean(roc_auc)
 
 
-def train(model, data_loader, loss_function, optimizer, weight_pos, weight_neg, device):
+def train(model, data_loader, loss_function, optimizer, device):
     """
         It trains the model for one epoch
     :param model: model we need to train
@@ -119,15 +212,19 @@ def train(model, data_loader, loss_function, optimizer, weight_pos, weight_neg, 
 
         total_number += image_input.shape[0]
         total_loss += image_input.shape[0] * loss.item()
+
         mean_auc.append(mean_roc_auc(
             targets.detach().cpu(), outputs.detach().cpu()))
+
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
+
     return total_loss / total_number, np.average(mean_auc)
 
 
-def test(model, data_loader, loss_function, weight_pos, weight_neg, device):
+def test(model, data_loader, loss_function, device):
     with torch.no_grad():
         model.eval()
         total_number = 0
@@ -147,6 +244,137 @@ def test(model, data_loader, loss_function, weight_pos, weight_neg, device):
 
 def get_current_dir():
     return os.path.dirname(os.path.realpath(__file__))
+
+
+def main_train(cfg, model_name, verbose=False):
+    device = get_device()
+
+    data = get_data()
+    if data:
+        train_data = data.sample(frac=cfg["TRAIN_VALIDATION_FRAC"])
+        validation_data = data.drop(train_data.index)
+    else:
+        return
+    train_transform = get_transform(image_size=cfg["IMAGE_SIZE"][model_name], augmented=True)
+    validation_transform = get_transform(image_size=cfg["IMAGE_SIZE"][model_name])
+
+    train_dataset = DatasetTransformer(train_data, transform=train_transform, cfg=cfg)
+    validation_dataset = DatasetTransformer(
+        validation_data, transform=validation_transform, cfg=cfg)
+
+    train_dataset_loader = torch.utils.data.DataLoader(
+        dataset=train_dataset, batch_size=cfg["BATCH_SIZE"], shuffle=True)
+    validation_dataset_loader = torch.utils.data.DataLoader(
+        dataset=validation_dataset, batch_size=cfg["BATCH_SIZE"], shuffle=True)
+
+    if model_name == "resnet":
+        #TODO
+        pass
+    elif model_name == "efficientnet":
+        model = EfficientNet(cfg=cfg, model_name=model_name)
+    elif model_name == "ensemble":
+        #TODO
+        pass
+    else:
+        print("Wrong model name")
+        return 
+
+    model.to(device)
+
+    if verbose:
+        summary(model, (3, cfg["IMAGE_SIZE"], cfg["IMAGE_SIZE"]))
+
+    optimizer = torch.optim.Adam(
+        model.parameters(), lr=cfg["LR"], weight_decay=cfg["WEIGHT_DECAY"])
+
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer=optimizer, patience=3, factor=0.5, verbose=True, mode='min', threshold=2*1e-2)
+
+    loss = nn.BCELoss()
+
+    train_updater = GraphUpdater(type="Train")
+    validation_updater = GraphUpdater(type="Validation")
+    model_checkpoint = ModelCheckpoint(model)
+
+    print(f"Training on {len(train_dataset)} images")
+
+    for t in range(cfg["EPOCHS"]):
+        print(f'Epoch: {t}')
+
+        train_loss, train_auc = train(model=model, data_loader=train_dataset_loader, loss_function=loss,
+                                      optimizer=optimizer, device=device)
+
+        train_updater.update(**{"loss": train_loss, "accuracy": train_auc})
+
+        print(f'\tTraining step: Loss: {train_loss}, AUC: {train_auc}', end='\n')
+
+        val_loss, val_auc = test(model=model, data_loader=validation_dataset_loader,
+                                 loss_function=loss, device=device)
+        validation_updater.update(**{"loss": val_loss, "accuracy": val_auc})
+
+        print(f'\tValidation step: Loss: {val_loss}, AUC: {val_auc}', end='\n')
+
+        torch.save(model, f'checkpoint_epoch_{t}.pth')
+        print('\tModel saved')
+
+        model_checkpoint.update(loss=val_loss)
+        scheduler.step(val_loss)
+
+    train_updater.display()
+    validation_updater.display()
+
+
+def get_prediction(model, test_dataset_loader, device):
+    with torch.no_grad():
+        model.eval()
+        probs = []
+        size = len(test_dataset_loader)
+        i = 0
+        for data in test_dataset_loader:
+            print(f"{int(i*100/size)}%", end="\r")
+            image_input = data["image"].to(device)
+            output_1 = model(image_input)
+            output_2 = model(image_input.flip(-1))
+            outputs = (output_1.to('cpu').numpy() + output_2.to('cpu').numpy()) / 2
+            probs.append(outputs)
+            i += 1
+        probs = np.concatenate(probs)
+    
+    return probs
+
+
+def main_predict(cfg, model_name, verbose=False):
+    try:
+        model_path = cfg["models"][model_name]
+        print(model_path)
+        model = torch.load(model_path)
+    except:
+        print("Wrong model name")
+        return
+
+    
+    device = get_device()
+    model.to(device)
+
+    df_test = pd.read_csv(f'{cfg["DATASET_DIR"]}/sample_submission.csv')
+
+    transform = get_transform(image_size=cfg["IMAGE_SIZE"][model_name])
+
+    test_dataset = DatasetTransformer_prediction(cfg=cfg, df=df_test, transform=transform)
+    test_dataset_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=cfg["BATCH_SIZE"], shuffle=True)
+    
+    if verbose:
+        summary(model, (3, cfg["IMAGE_SIZE"], cfg["IMAGE_SIZE"]))
+    
+    predictions = get_prediction(model, test_dataset_loader, device)
+
+    df_test[cfg["TARGET_COLS"]] = predictions
+    df_test[[cfg["IMAGE_COL"]] + cfg["TARGET_COLS"]].to_csv('submission.csv', index=False)
+    
+    if verbose:
+        print(df_test.head(20))
+
+
 
 
 class GraphUpdater():
